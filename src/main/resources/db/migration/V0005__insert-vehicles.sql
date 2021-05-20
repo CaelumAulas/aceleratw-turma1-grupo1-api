8ef155c2fd5f5a7ef2978fa823d3ee88103db572
vitor nascimento <vitor.nascimento@thoughtworks.com>
2021-05-20 14:53:43 -0300

    FeatLogin(UserService)

---
+++ src/main/resources/db/migration/V0005__insert-vehicles.sql
@@ -0,0 +1,3 @@
+INSERT INTO vehicle(id,modelo,ano,valor) VALUES (1,'ka',2007, 10000);
+INSERT INTO vehicle(id,modelo,ano,valor) VALUES (2,'punto',2014, 10000);
+INSERT INTO vehicle(id,modelo,ano,valor) VALUES (3,'gol',2012, 10000);
